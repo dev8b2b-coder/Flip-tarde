df679b940140f5eefa5bb97c92cb9d71bc523941
techbase2brand <techbase2brand@gmail.com>
2025-10-13 17:39:44 +0530

    web trading final

--- src/app/web-trading/Starttradinggraph.jsx
+++ src/app/web-trading/Starttradinggraph.jsx
@@ -5,14 +5,14 @@ import React from 'react';
 
 export default function Starttradinggraph() {
   return (
-    <div className='bg-black container m-auto'>
+    <div className='bg-black inn_container m-auto'>
       <iframe
         src="https://webtrading.fliptradegroup.com/terminal"
         frameBorder="0"
         allow="fullscreen; clipboard-read; clipboard-write"
         scrolling="yes"
         width='100%'
-        height='500'
+        height='700'
         loading="lazy"
         style={{
           border: '1px solid #ccc',
